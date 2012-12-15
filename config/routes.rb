005bc6ff79b9bd579cec7c80a20eca20942ecd22
Hernan Sarmiento <hernan.sarmientoa@gmail.com>
2012-12-15 16:55:07 -0300

    Finish layout and router

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 MyApp::Application.routes.draw do
+
+  root to: 'static_pages#home'
+  get "users/new"
+
+  match 'signup', to: 'users#new'
   match '/', to: 'static_pages#home'
   match '/home', to: 'static_pages#home'
   match '/help', to: 'static_pages#help'
   match '/contact', to: 'static_pages#contact'
-  root to: 'static_pages#home'
+
+
   resources :microposts
 
   resources :users
