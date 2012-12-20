a51f8b33e56053c6891835d008564520f8bed75a
Hernan Sarmiento <hernan.sarmientoa@gmail.com>
2012-12-20 14:58:38 -0300

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,21 @@
 MyApp::Application.routes.draw do
 
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
 
   root to: 'static_pages#home'
   #get "users/new"
 
-  match 'signup', to: 'users#new'
+  match '/signup', to: 'users#new'
+  match '/signin',  to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
+
   match '/', to: 'static_pages#home'
   match '/home', to: 'static_pages#home'
   match '/help', to: 'static_pages#help'
   match '/contact', to: 'static_pages#contact'
 
+ 
 
   #resources :microposts
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121217143451) do
+ActiveRecord::Schema.define(:version => 20121218203815) do
 
   create_table "microposts", :force => true do |t|
     t.string   "content"
@@ -27,8 +27,10 @@ ActiveRecord::Schema.define(:version => 20121217143451) do
     t.datetime "updated_at",       :null => false
     t.string   "password_diggest"
     t.string   "password_digest"
+    t.string   "remember_token"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+  add_index "users", ["remember_token"], :name => "index_users_on_remember_token"
 
 end
